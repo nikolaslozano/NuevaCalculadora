2b5dfd12c78b5fac17584c5a18b1356869e0345a
Nicolás Lozano Aroca <nlozanoa@correo.udistrital.edu.co>
2018-08-17 11:23:26 -0500

    Más en el conversor

--- Calculadora/Conversor.java
+++ Calculadora/Conversor.java
@@ -18,5 +18,47 @@ public class Conversor {
     }
     double fahrenheitToKelvin(){
         return (unidades+459.67)*(5/9);
-    }   
-}
+    }
+    double gramToOunce(){
+        return unidades/28.3495;
+    }
+    double ounceToGram(){
+        return unidades*28.3495;
+    }
+    double poundToGram(){
+        return unidades*453.592;
+    }
+    double gramToPound(){
+        return unidades/453.592;
+    }
+    double tonneToKilogramAndKtG(){
+        return unidades*1000;
+    }
+    double kilogramToTonneAndGtK(){
+        return unidades/1000;
+    }
+    double metresToFeet(){
+        return unidades*3.28084;   
+    }
+    double feetToMetres(){
+        return unidades/3.28084;
+    }
+    double inchesToFeet(){
+        return unidades/12;
+    }
+    double feetToInches(){
+        return unidades*12;
+    }
+    double centimetresToMetres(){
+        return unidades/100;
+    }
+    double metresToCentimetres(){
+        return unidades*100;
+    }
+    double milesToMetres(){
+        return unidades*1609.34;
+    }
+    double metresToMiles(){
+        return unidades/1609.34;
+    }
+}   
