5b8504ea79a5e4950427784cad956529323fe5b8
Nicolás Lozano Aroca <nlozanoa@correo.udistrital.edu.co>
2018-08-31 09:12:50 -0500

    Otros

--- src/Calculadora/Ultracalculadora.java
+++ src/Calculadora/Ultracalculadora.java
@@ -4,14 +4,21 @@ import java.util.Scanner;
 
 public class Ultracalculadora {
     
+    CalculadoraAritmética miCalculadora=new CalculadoraAritmética();
+    CalculadoraTrigonométrica ángulos=new CalculadoraTrigonométrica();
+    Conversor unidades=new Conversor();
+    Scanner teclado=new Scanner(System.in);
+    int numero=teclado.nextInt();
+        
+        void ultraCalculadora (){
+            System.out.println("Seleccione una opción:\n1.Aritmética\n2.Trigonometría\n3.Conversor\n4.Salir");
+            teclado.nextInt();
+        }
+    
     public static void main(String[]args){
         
-        CalculadoraAritmética miCalculadora=new CalculadoraAritmética();
-        CalculadoraTrigonométrica ángulos=new CalculadoraTrigonométrica();
-        Conversor unidades=new Conversor();
-        Scanner teclado=new Scanner(System.in);
-        teclado.nextInt();
-        int numero=teclado.nextInt();
+        
+        
         
     }
 }
